fe42423a26738cee6ea80ac1465069feee3e7d76
David Eliason <davethemaker@gamil.com>
2017-06-08 19:02:03 -0700

    complete list method and controller has been exported, my hope is to test just this before adding other things of CRUD nature

--- controllers/FriendController.js
+++ controllers/FriendController.js
@@ -1,2 +1,17 @@
 const mongoose = require('mongoose');
 const Friend = mongoose.model("Friend");
+
+const friendController = {};
+
+friendController.list = function(req,res){
+	Friend.find({}).exec(function(err, friends){
+		if(err){
+			console.log("Error");
+		}
+		else{
+			res.render('../views/friends/index', {friends:friends});
+		}
+	});
+};
+
+module.exports = friendController;
