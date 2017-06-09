3a1ba9976093ce205512430f0b8230179cbf9d5b
David Eliason <davethemaker@gamil.com>
2017-06-09 07:01:53 -0700

    add route for deleting a friend

--- routes/friends.js
+++ routes/friends.js
@@ -11,22 +11,25 @@ router.get('/', function(req,res){
 router.get('/show/:id', function(req,res){
 	friend.show(req,res);
 });
-
-// create friend
+// create friend with form
 router.get('/create', function(req,res){
 	friend.create(req,res);
 });
-
+// edit specific friend
 router.post('/update/:id', function(req,res){
 	friend.update(req,res);
 });
-
+// save new friend to db
 router.post('/save', function(req,res){
 	friend.save(req,res);
 });
-
+// edit friend
 router.get('/edit/:id', function(req,res){
 	friend.edit(req,res);
+});
+
+router.post('/delete/:id', function(req,res,next){
+	friend.delete(req,res);
 });
 // export module
 module.exports = router;
