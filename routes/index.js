28427d2052acbef976c54b0792f283e5f75c7bd2
David Eliason <davethemaker@gamil.com>
2017-06-09 08:06:30 -0700

    fixed bug that was setting entire About page as a link

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,8 @@ router.get('/', function(req, res, next){
 	res.render('index', {title: 'Friends'});
 });
 
+router.get('/about', function(req,res,next){
+	res.render('about', {title: 'About'});
+});
+
 module.exports = router;
