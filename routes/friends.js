3d25e637b2a00ed7a935d613ff8c57445d4cf7a8
David Eliason <davethemaker@gamil.com>
2017-06-09 05:28:25 -0700

    add routes for create, save to previous show

--- routes/friends.js
+++ routes/friends.js
@@ -7,4 +7,12 @@ router.get('/', function(req,res){
 	friend.list(req,res);
 });
 
+// show single friend by id
+route.get('/show/:id', friend.show);
+
+// create friend
+route.get('/create', friend.create);
+
+router.post('/save', friend.save);
+// export module
 module.exports = router;
